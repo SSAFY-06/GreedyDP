b7d2ccc041c4cb39f60794b7c48ff3c3d6c3c87e
kimsuhyeok <tngur1101@naver.com>
2023-09-03 15:49:29 +0900

    퇴사2
    퇴사2는 입력값이 커서 dynamic programming으로 접근해야한다.

--- kimsuhyeok/BOJ_15486_퇴사2.java
+++ kimsuhyeok/BOJ_15486_퇴사2.java
@@ -1,8 +1,40 @@
 package kimsuhyeok;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_15486_퇴사2 {
 
-    public static void main(String[] args) {
+    static int N;
+    static int[] t, p, dp;
 
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        t = new int[N+1];
+        p = new int[N+1];
+
+        for(int i=1;i<=N;i++){
+            st = new StringTokenizer(br.readLine());
+
+            t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp = new int[N+1];
+
+        for(int i=0;i<N;i++){
+            if(i+t[i]<=N){
+                dp[i+t[i]] = Math.max(dp[i+t[i]], dp[i]+p[i]);
+            }
+            dp[i+1] = Math.max(dp[i+1], dp[i]);
+        }
+
+
+        System.out.println(dp[N]);
     }
 }
