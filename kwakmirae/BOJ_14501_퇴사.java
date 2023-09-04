ba7c8ab1027a1bc4fd769c36ff36bc3e9202b5d8
miraekwak <alfo9490@naver.com>
2023-09-04 17:09:45 +0900

    BOJ_14501_퇴사

---
+++ kwakmirae/BOJ_14501_퇴사.java
@@ -0,0 +1,30 @@
+package kwakmirae;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_14501_퇴사 {
+
+	public static void main(String[] args) throws Exception, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] time = new int[N];
+		int[] profit = new int[N];
+		StringTokenizer st;
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			time[i] = Integer.parseInt(st.nextToken());
+			profit[i] = Integer.parseInt(st.nextToken());
+		}
+		int[] dp = new int[N+1];
+		for(int i=0; i<N; i++) {
+			if(i+time[i] <= N) {
+				dp[i+time[i]] = Math.max(dp[i+time[i]], dp[i]+profit[i]);
+			}
+			dp[i+1] = Math.max(dp[i+1], dp[i]);
+		}
+		System.out.println(dp[N]);
+	}
+}
